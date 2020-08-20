42e1b767d90dbcc8845456a13aac328e6c02db65
Andrew S <andrew@andrewstiefel.com>
2020-08-20 08:52:31 -0700

    Custom auth check in header

---
+++ custom-auth-check.js
@@ -0,0 +1,26 @@
+var paywall = new InplayerPaywall("1e87268a-4dd7-46ef-a39f-3a8e193f7173",
+     [
+      {
+        id: 111402,
+        options: {
+            noPreview: true,
+            noInject: true
+        }
+      }]
+    );
+
+
+// Check if the user is authenticated first
+
+document.addEventListener('DOMContentLoaded', () => {
+
+var authStatus = new paywall.isAuthenticated();
+
+if(authStatus == true){
+
+    document.getElementById("custom-login").style.display = "none";
+    document.getElementById("custom-register").style.display = "none";
+    document.getElementById("custom-account").style.display = "block";
+    document.getElementById("inplayer-menu-content").style.display = "none";
+  }
+});
--- custom-bundle.js
+++ custom-bundle.js
@@ -1,20 +1,5 @@
 var paywall = new InplayerPaywall("1e87268a-4dd7-46ef-a39f-3a8e193f7173");
 
-// Check if the user is authenticated first
-
-document.addEventListener('DOMContentLoaded', () => {
-
-var authStatus = new paywall.isAuthenticated();
-
-if(authStatus == true){
-
-    document.getElementById("custom-login").style.display = "none";
-    document.getElementById("custom-register").style.display = "none";
-    document.getElementById("custom-account").style.display = "block";
-    document.getElementById("inplayer-menu-content").style.display = "none";
-  }
-});
-
 // Hide register and login buttons after successful authentication
 
   document.getElementById("custom-login").addEventListener("click", () => {
