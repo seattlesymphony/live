441674832bbb0fe2120800494d97dbcb39f02a63
Andrew S <andrew@andrewstiefel.com>
2020-08-20 17:40:59 -0700

    Comment out auth check and move to unique embed code for header

--- custom-auth-check.js
+++ custom-auth-check.js
@@ -1,13 +1,13 @@
 var paywall = new InplayerPaywall("1e87268a-4dd7-46ef-a39f-3a8e193f7173",
-     [
-      {
-        id: 111402,
-        options: {
-            noPreview: true,
-            noInject: true
-        }
-      }]
-    );
+  [{
+    id: 111402,
+    options: {
+      noPreview: true,
+      noInject: true
+    }
+  }], {
+    hideUserMenu: true
+});
 
 // Check if the user is authenticated first
 
@@ -20,6 +20,6 @@ if(authStatus == true){
     document.getElementById("custom-login").style.display = "none";
     document.getElementById("custom-register").style.display = "none";
     document.getElementById("custom-account").style.display = "block";
-    document.getElementById("inplayer-menu-content").style.display = "none";
+
   }
 });
--- custom-bundle.js
+++ custom-bundle.js
@@ -31,18 +31,18 @@ var paywall = new InplayerPaywall("1e87268a-4dd7-46ef-a39f-3a8e193f7173");
 
   // Check if the user is authenticated
 
-  document.addEventListener('DOMContentLoaded', () => {
+  // document.addEventListener('DOMContentLoaded', () => {
 
-  var authStatus = new paywall.isAuthenticated();
+  // var authStatus = new paywall.isAuthenticated();
 
-  if(authStatus == true){
+  // if(authStatus == true){
 
-      document.getElementById("custom-login").style.display = "none";
-      document.getElementById("custom-register").style.display = "none";
-      document.getElementById("custom-account").style.display = "block";
-      document.getElementById("inplayer-menu-content").style.display = "none";
-    }
-  });
+      // document.getElementById("custom-login").style.display = "none";
+      // document.getElementById("custom-register").style.display = "none";
+      // document.getElementById("custom-account").style.display = "block";
+      // document.getElementById("inplayer-menu-content").style.display = "none";
+    // }
+  // });
 
   // Open and close mobile navmenu
 
